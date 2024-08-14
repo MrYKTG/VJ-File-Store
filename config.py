6a776c4a584d8721a1a7f6811ee6176ae0eaf27e
Mr kuttusan <yedhukrishnanu2001@gmail.com>
2024-08-14 13:29:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ CDB_NAME = environ.get("CDB_NAME", "Xeonfilestoreclone")
 
 # Database Information
 DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "Xeonfilestore")
+DB_NAME = environ.get("DB_NAME", "Xeonfile")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
